90f66b577bd0b222a169a2ef393598b0fc469722
gyoungjun_lee <injxgj@gmail.com>
2025-01-30 12:26:55 +0900

    feat: 헤더 영역 레이아웃 수정 & external_link아이콘 추가

---
+++ src/components/Layout/styles.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import { Flex } from "../common/Flex";
+
+export const Wrapper = styled(Flex)`
+  height: 100vh;
+  background-color: rgb(248, 248, 248);
+`;
+
+export const Header = styled(Flex)`
+  width: 92%;
+  border-bottom: 1px solid #e0e0e0;
+`;
