92b3c4746109e031cc1750badb6878cdcabae7da
gyoungjun_lee <injxgj@gmail.com>
2025-01-30 23:11:21 +0900

    feat: 헤더, 메인 레이아웃 기본적인 반응형 적용

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,71 @@
+import styled from "@emotion/styled";
+import { Flex } from "../common/Flex";
+import Link from "next/link";
+import { Typography } from "../common/Typography";
+import MenuIcon from "@/assets/icons/menu.svg";
+import ExternalLinkIcon from "@/assets/icons/external-link.svg";
+import { useBreakpoints } from "@/hooks/useBreakpoints";
+import { useState, useEffect } from "react";
+
+const Wrapper = styled(Flex)`
+  height: 60px;
+  padding: 0 32px;
+`;
+
+const portfolioLink =
+  "https://serious-curler-ce2.notion.site/98d45e81d19046bea98d6803e6cb4d3e?pvs=4";
+
+export const Header = () => {
+  const { isMobile } = useBreakpoints();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  if (isMobile) {
+    return (
+      <>
+        <Wrapper justify="space-between" align="center">
+          <Link href="/posts">
+            <Typography as="h1" variant="title4">
+              딸 아빠 개발자
+            </Typography>
+          </Link>
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <MenuIcon width={24} height={24} />
+          </button>
+        </Wrapper>
+        {isOpen && (
+          <a href={portfolioLink} target="_blank" style={{ width: "100%" }}>
+            <Flex
+              align="center"
+              justify="center"
+              css={{ height: "40px", width: "100%" }}
+            >
+              <Typography as="span" variant="body2">
+                포트폴리오
+              </Typography>
+              <ExternalLinkIcon width={20} height={20} />
+            </Flex>
+          </a>
+        )}
+      </>
+    );
+  }
+
+  return (
+    <Wrapper justify="space-between" align="center">
+      <Link href="/posts">
+        <Typography as="h1" variant="title4">
+          딸 아빠 개발자
+        </Typography>
+      </Link>
+      <a href={portfolioLink} target="_blank">
+        <Flex justify="flex-end" align="center" gap="4px">
+          <Typography as="p" variant="body2">
+            포트폴리오
+          </Typography>
+          <ExternalLinkIcon width={20} height={20} />
+        </Flex>
+      </a>
+    </Wrapper>
+  );
+};
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,57 +1,24 @@
-import Link from "next/link";
 import { Flex } from "../common/Flex";
-import { Typography } from "../common/Typography";
 import { Profile } from "../Profile";
-import { Aside, Header, Main, Wrapper } from "./styles";
-import ExternalLinkIcon from "@/assets/icons/external-link.svg";
-import MenuIcon from "@/assets/icons/menu.svg";
+import { Aside, Main, Wrapper } from "./styles";
 import { useBreakpoints } from "@/hooks/useBreakpoints";
+import { Header } from "./Header";
 
-const portfolioLink =
-  "https://serious-curler-ce2.notion.site/98d45e81d19046bea98d6803e6cb4d3e?pvs=4";
-
-export const Layout = ({
-  children,
-  categories,
-}: {
-  children: React.ReactNode;
-  categories: string[];
-}) => {
-  const { isMobile } = useBreakpoints();
-  //   const isMobile = false;
-
-  console.log(ExternalLinkIcon);
-
+export const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { isTablet, isMobile } = useBreakpoints();
   return (
     <Wrapper direction="column" align="center">
       <div style={{ width: "100%", borderBottom: "1px solid #e0e0e0" }}>
-        <Header justify="space-between" align="center">
-          <Link href="/posts">
-            <Typography as="h1" variant="title4">
-              딸 아빠 개발자
-            </Typography>
-          </Link>
-
-          {isMobile ? (
-            <MenuIcon width={24} height={24} />
-          ) : (
-            <a href={portfolioLink} target="_blank">
-              <Flex justify="flex-end" align="center" gap="4px">
-                <Typography as="p" variant="body2">
-                  포트폴리오
-                </Typography>
-                <ExternalLinkIcon width={20} height={20} />
-              </Flex>
-            </a>
-          )}
-        </Header>
+        <Header />
       </div>
 
       <Flex justify="center" css={{ maxWidth: "1200px" }}>
         <Main as="main">{children}</Main>
-        <Aside as="aside" direction="column">
-          <Profile />
-        </Aside>
+        {!isMobile && !isTablet && (
+          <Aside as="aside" direction="column">
+            <Profile />
+          </Aside>
+        )}
       </Flex>
     </Wrapper>
   );
