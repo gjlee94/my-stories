50b0e711bd7ce479be20be336f77ad872fa84b78
gyoungjun_lee <injxgj@gmail.com>
2025-01-31 13:56:52 +0900

    feat: tablist 컴포넌트 추가

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,27 @@
 import { Flex } from "../common/Flex";
 import { Profile } from "../Profile";
-import { Aside, Main, Wrapper } from "./styles";
 import { useBreakpoints } from "@/hooks/useBreakpoints";
 import { Header } from "./Header";
+import styled from "@emotion/styled";
+
+export const Wrapper = styled(Flex)`
+  height: 100vh;
+  background-color: rgb(248, 248, 248);
+`;
+
+export const Main = styled(Flex)`
+  max-width: 800px;
+  flex: 1 1 800px;
+
+  padding: 24px;
+`;
+
+export const Aside = styled(Flex)`
+  min-width: 300px;
+  flex: 1 1 300px;
+
+  padding: 24px;
+`;
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   const { isTablet, isMobile } = useBreakpoints();
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,6 +3,11 @@ import { getAllPosts } from "@/lib/posts";
 import type { Post } from "@/types/post";
 import { PreviewContent } from "@/components/PreviewContent";
 import Link from "next/link";
+import styled from "@emotion/styled";
+import { Typography } from "@/components/common/Typography";
+import { css } from "@emotion/react";
+import { useState } from "react";
+import { TabList } from "@/components/TabList";
 
 export async function getStaticProps() {
   const posts = getAllPosts();
@@ -18,6 +23,8 @@ interface PostsPageProps {
 }
 
 export default function PostsPage({ posts }: PostsPageProps) {
+  const tabs = ["전체", ...new Set(posts.map((post) => post.category))];
+  const [selectedTab, setSelectedTab] = useState("전체");
   return (
     <Flex
       direction="column"
@@ -25,11 +32,22 @@ export default function PostsPage({ posts }: PostsPageProps) {
       gap={20}
       css={{ padding: "20px 0" }}
     >
-      {posts.map((post) => (
-        <Link key={post.slug} href={`/posts/${post.slug}`}>
-          <PreviewContent post={post} />
-        </Link>
-      ))}
+      <TabList
+        tabs={tabs}
+        selectedTab={selectedTab}
+        onTabClick={setSelectedTab}
+      />
+      {posts
+        .filter((post) => {
+          if (selectedTab === "전체") return true;
+
+          return post.category === selectedTab;
+        })
+        .map((post) => (
+          <Link key={post.slug} href={`/posts/${post.slug}`}>
+            <PreviewContent post={post} />
+          </Link>
+        ))}
     </Flex>
   );
 }
