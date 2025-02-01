e8e1a6f8992afc834ff677606e14216dc276bc90
gyoungjun_lee <injxgj@gmail.com>
2025-02-01 21:44:21 +0900

    chore: next build를 위한 웹팩 설정 추가

--- next.config.js
+++ next.config.js
@@ -5,13 +5,21 @@ const nextConfig = {
   compiler: {
     emotion: true,
   },
+  /** next build 실행 시 Webpack이 최종 번들링을 수행함. */
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  /** 개발 환경에서의 빌드 속도를 극대화하기 위해 Turbopack이 사용됨. */
   experimental: {
     turbo: {
       resolveAlias: {
         "@": path.resolve(process.cwd(), "src"),
       },
       rules: {
-        // SVG를 React 컴포넌트로 변환 (중복 파일명 방지)
         "*.svg": {
           loaders: ["@svgr/webpack"],
           as: "*.js",
