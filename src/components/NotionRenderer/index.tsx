ff0f47275c30580de6512b03f470f7d27733add4
gyoungjun_lee <injxgj@gmail.com>
2025-02-05 17:37:14 +0900

    feat: NotionRenderer 커스텀 컴포넌트 추가

---
+++ src/components/NotionRenderer/index.tsx
@@ -0,0 +1,60 @@
+import dynamic from "next/dynamic";
+import Image from "next/image";
+
+// core styles shared by all of react-notion-x (required)
+import "react-notion-x/src/styles.css";
+
+// used for code syntax highlighting (optional)
+import "prismjs/themes/prism-tomorrow.css";
+import Link from "next/link";
+
+const _NotionRenderer = dynamic(
+  () => import("react-notion-x").then((m) => m.NotionRenderer),
+  { ssr: false }
+);
+
+const Code = dynamic(() =>
+  import("react-notion-x/build/third-party/code").then(async (m) => m.Code)
+);
+
+const Collection = dynamic(() =>
+  import("react-notion-x/build/third-party/collection").then(
+    (m) => m.Collection
+  )
+);
+const Equation = dynamic(() =>
+  import("react-notion-x/build/third-party/equation").then((m) => m.Equation)
+);
+const Pdf = dynamic(
+  () => import("react-notion-x/build/third-party/pdf").then((m) => m.Pdf),
+  {
+    ssr: false,
+  }
+);
+const Modal = dynamic(
+  () => import("react-notion-x/build/third-party/modal").then((m) => m.Modal),
+  {
+    ssr: false,
+  }
+);
+
+interface NotionRendererProps {
+  posts: any;
+}
+
+export default function NotionRenderer({ posts }: NotionRendererProps) {
+  return (
+    <_NotionRenderer
+      recordMap={posts}
+      components={{
+        Code,
+        Collection,
+        Equation,
+        Modal,
+        Pdf,
+        nextImage: Image,
+        nextLink: Link,
+      }}
+    />
+  );
+}
