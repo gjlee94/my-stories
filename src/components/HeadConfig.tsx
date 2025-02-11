27c91b9877fe00322cb36ba4c27d4ed5a5f53e0a
gyoungjun_lee <injxgj@gmail.com>
2025-02-11 14:51:15 +0900

    fix: optional propery 처리

--- src/components/HeadConfig.tsx
+++ src/components/HeadConfig.tsx
@@ -14,7 +14,7 @@ export const HeadConfig = ({ title, summary, tags, slug }: HeadConfigProps) => {
       <meta name="description" content={summary} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="robots" content="index, follow"></meta>
-      <meta name="keywords" content={tags.join(", ")} />
+      {tags && <meta name="keywords" content={tags.join(", ")} />}
 
       <meta property="og:title" content={title} />
       <meta property="og:description" content={summary} />
