f93e0a7788cc1887d2b5d3fbe32083cdc62eafc8
gyoungjun_lee <injxgj@gmail.com>
2025-02-11 23:48:41 +0900

    feat: 헤더 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,6 @@ import styled from "@emotion/styled";
 import { Flex } from "./common/Flex";
 import Link from "next/link";
 import { Typography } from "./common/Typography";
-import { useBreakpoints } from "@/hooks/useBreakpoints";
-import { useState } from "react";
 import Image from "next/image";
 
 const Wrapper = styled(Flex)`
@@ -15,56 +13,11 @@ const portfolioLink =
   "https://serious-curler-ce2.notion.site/98d45e81d19046bea98d6803e6cb4d3e?pvs=4";
 
 export const Header = () => {
-  const { isMobile } = useBreakpoints();
-
-  const [isOpen, setIsOpen] = useState(false);
-
-  if (isMobile) {
-    return (
-      <>
-        <Wrapper justify="space-between" align="center">
-          <Link href="/">
-            <Typography as="h1" variant="title4">
-              딸 아빠 개발자
-            </Typography>
-          </Link>
-          <button onClick={() => setIsOpen(!isOpen)}>
-            <Image
-              src="/assets/icons/menu.svg"
-              alt="menu"
-              width={24}
-              height={24}
-            />
-          </button>
-        </Wrapper>
-        {isOpen && (
-          <a href={portfolioLink} target="_blank" style={{ width: "100%" }}>
-            <Flex
-              align="center"
-              justify="center"
-              css={{ height: "40px", width: "100%" }}
-            >
-              <Typography as="span" variant="body2">
-                포트폴리오
-              </Typography>
-              <Image
-                src="/assets/icons/external-link.svg"
-                alt="external-link"
-                width={20}
-                height={20}
-              />
-            </Flex>
-          </a>
-        )}
-      </>
-    );
-  }
-
   return (
     <Wrapper justify="space-between" align="center">
       <Link href="/">
         <Typography as="h1" variant="title4">
-          딸 아빠 개발자
+          Gyoungjun's Blog
         </Typography>
       </Link>
       <a href={portfolioLink} target="_blank">
