896cf2c693f12791db459285599aecee66756585
gyoungjun_lee <injxgj@gmail.com>
2025-02-13 23:50:09 +0900

    fix: static html 파일에 맞는 경로로 수정

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -20,3 +20,4 @@ export function logout() {
   sessionStorage.removeItem("authToken");
   window.location.reload();
 }
+https://dz8loj6wyz9a4.cloudfront.net/auth/callback#error_description=Response+from+IdP+token+endpoint+cannot+be+parsed+as+JSON+or+has+an+invalid+format.&error=invalid_request
