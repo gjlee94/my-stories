e2d52b2d174afb71a1f4de2db4aa94e7b1406d27
gyoungjun_lee <injxgj@gmail.com>
2025-02-24 16:45:37 +0900

    feat: 툴팁 추가

--- src/components/EmoticonBox/Emoticon.tsx
+++ src/components/EmoticonBox/Emoticon.tsx
@@ -8,12 +8,50 @@ export const emoticonSet: Record<EmoticonType, string> = {
   heart: "❤️",
 };
 
+const Wrapper = styled.div`
+  position: relative;
+`;
+
 const StyledEmoticon = styled.div<{ active?: boolean }>`
   cursor: pointer;
+  position: relative;
 
   ${({ active }) => active && "transform: scale(1.1);"}
-  &:hover {
+  ${Wrapper}:hover & {
     transform: scale(1.1);
+  }
+`;
+
+const Tooltip = styled.div`
+  position: absolute;
+  bottom: calc(100% + 8px);
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 8px;
+  background-color: rgba(0, 0, 0, 0.8);
+  color: white;
+  border-radius: 4px;
+  font-size: 12px;
+  z-index: 1;
+  display: none;
+  width: 120px;
+  white-space: pre-wrap;
+  word-break: break-all;
+
+  // 툴팁 화살표
+  &::after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+    border-width: 5px;
+    border-style: solid;
+    border-color: rgba(0, 0, 0, 0.8) transparent transparent transparent;
+  }
+
+  ${Wrapper}:hover & {
+    display: block;
   }
 `;
 
@@ -32,10 +70,17 @@ export const Emoticon = ({
   onClick?: () => void;
   disabled?: boolean;
 }) => {
+  const tooltipText = clickedBy ? clickedBy : "아직 없음";
+
   return (
-    <Tag active={active} onClick={onClick} disabled={disabled}>
-      <StyledEmoticon active={active}>{emoticonSet[type]}</StyledEmoticon>
-      <span>{count}</span>
-    </Tag>
+    <Wrapper>
+      <Tag active={active} onClick={onClick} disabled={disabled}>
+        <StyledEmoticon active={active}>
+          {emoticonSet[type]}
+          <Tooltip>{tooltipText}</Tooltip>
+        </StyledEmoticon>
+        <span>{count}</span>
+      </Tag>
+    </Wrapper>
   );
 };
