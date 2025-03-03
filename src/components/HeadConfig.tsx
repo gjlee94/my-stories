a8d0f593914f788220ed83a1b184005fc838d317
gyoungjun_lee <injxgj@gmail.com>
2025-03-03 19:58:35 +0900

    feat: ads.txt추가

--- src/components/HeadConfig.tsx
+++ src/components/HeadConfig.tsx
@@ -10,36 +10,28 @@ interface HeadConfigProps {
 
 export const HeadConfig = ({ title, summary, tags, slug }: HeadConfigProps) => {
   return (
-    <>
-      <Head>
-        <title>{title}</title>
-        <meta name="description" content={summary} />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="robots" content="index, follow" />
-        {tags && <meta name="keywords" content={tags.join(", ")} />}
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={summary} />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="robots" content="index, follow" />
+      {tags && <meta name="keywords" content={tags.join(", ")} />}
 
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={summary} />
-        <meta
-          property="og:image"
-          content={`${process.env.NEXT_PUBLIC_SITE_URL}/assets/profile.png`}
-        />
-        <meta
-          property="og:url"
-          content={
-            slug
-              ? `${process.env.NEXT_PUBLIC_SITE_URL}/${slug}`
-              : `${process.env.NEXT_PUBLIC_SITE_URL}`
-          }
-        />
-        <meta property="og:type" content="website" />
-      </Head>
-      <Script
-        strategy="afterInteractive"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        data-ad-client="ca-pub-9393302787267646"
-        crossOrigin="anonymous"
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={summary} />
+      <meta
+        property="og:image"
+        content={`${process.env.NEXT_PUBLIC_SITE_URL}/assets/profile.png`}
       />
-    </>
+      <meta
+        property="og:url"
+        content={
+          slug
+            ? `${process.env.NEXT_PUBLIC_SITE_URL}/${slug}`
+            : `${process.env.NEXT_PUBLIC_SITE_URL}`
+        }
+      />
+      <meta property="og:type" content="website" />
+    </Head>
   );
 };
