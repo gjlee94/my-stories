08d277b4bc59c76f2031cc3d32999098269438e2
gyoungjun_lee <injxgj@gmail.com>
2025-03-03 19:47:08 +0900

    feat: 코드 내에 이전 url 잔존하던 것 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 export default {
-  siteUrl: "https://dz8loj6wyz9a4.cloudfront.net", // 🚀 실제 도메인으로 변경
+  siteUrl: "https://gyoungjun-log.me/", // 🚀 실제 도메인으로 변경
   generateRobotsTxt: true, // ✅ robots.txt 자동 생성
   sitemapSize: 5000, // 한 개의 Sitemap에 포함될 최대 URL 개수
   exclude: [], // ❌ 검색에서 제외할 페이지
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,5 @@
 export function openLoginPopup() {
-  const loginUrl = `https://ap-northeast-2nzsznnb7w.auth.ap-northeast-2.amazoncognito.com/oauth2/authorize?client_id=7kr8uutulbgbs9jvldjhta97tb&response_type=token&scope=email%20openid%20profile&redirect_uri=https://dz8loj6wyz9a4.cloudfront.net/auth/callback`;
+  const loginUrl = `https://ap-northeast-2nzsznnb7w.auth.ap-northeast-2.amazoncognito.com/oauth2/authorize?client_id=7kr8uutulbgbs9jvldjhta97tb&response_type=token&scope=email%20openid%20profile&redirect_uri=https://gyoungjun-log.me/auth/callback`;
 
   const popup = window.open(loginUrl, "Login", "width=500,height=600");
 
