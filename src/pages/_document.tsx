410f774ec96c5bf8062fa300c8acadacf9dcee6c
gyoungjun_lee <injxgj@gmail.com>
2025-03-06 10:56:23 +0900

    feat: favicon 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,20 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-icon-180x180.png"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
