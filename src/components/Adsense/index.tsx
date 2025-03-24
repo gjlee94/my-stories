035cf0f2795cc2b7f6937e92c6f59d13ec695fe5
gyoungjun_lee <injxgj@gmail.com>
2025-03-24 11:55:31 +0900

    feat: adsense 광고 위치 고정

--- src/components/Adsense/index.tsx
+++ src/components/Adsense/index.tsx
@@ -28,11 +28,10 @@ const AdBanner = () => {
   return (
     <ins
       className="adsbygoogle"
-      style={{ display: "block", height: "150px" }}
+      style={{ display: "block", height: "150px", width: "100%" }}
       data-ad-client="ca-pub-9393302787267646"
       data-ad-slot="1234567890"
       data-adtest="on"
-      data-ad-format="auto"
       data-full-width-responsive="true"
     />
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -183,6 +183,7 @@ export default function PostsPage({
           </Aside>
         )}
       </Flex>
+      <AdSenseBanner />
     </>
   );
 }
